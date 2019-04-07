e6827dec19c44c1be3811cea849c3b3d8e1b7156
Connor Kennedy <cjtkennedy@gmail.com>
2019-04-07 18:48:52 -0500

    Tweak var names a bit

--- get-cached-weather.sh
+++ get-cached-weather.sh
@@ -9,6 +9,5 @@ if [[ $(cat $cache_file | wc -l) -eq 0 ]]; then
 	cache_weather
 fi
 
-# I'm piping this throuhg grep here because I don't want any
-# lines that don't start with the zip code such as '64131: ⛅️ +76°F'
-cat $cache_file | egrep '[0-9]' --color='none'
+# Deleting any lines that start with '<' to remove any error pages or any other HTML
+cat $cache_file | sed '/^</d'
